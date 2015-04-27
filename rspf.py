d98b213135b186e8a221954bd9dcf453629cabc7
Guillaume C. Morin <gmorin@sysif.net>
2015-04-27 09:50:28 +0200

    Refactoring

--- rspf.py
+++ rspf.py
@@ -3,6 +3,7 @@
 import sys
 import socket
 import dns.resolver
+import re
 
 if len(sys.argv) < 2:
     sys.stderr.write("Usage: %s domain\n" % sys.argv[0])
@@ -10,24 +11,24 @@ if len(sys.argv) < 2:
 
 domain=sys.argv[1]
 
-answers = dns.resolver.query(domain, 'TXT')
-for rdata in answers:
-  for txt_string in rdata.strings:
-    if txt_string.startswith('v=spf1'):
-      full_spf=txt_string.replace('v=spf1','')
-      print 'Initial SPF string : ', full_spf
+def getspf (domain):
+   answers = dns.resolver.query(domain, 'TXT')
+   for rdata in answers:
+     for txt_string in rdata.strings:
+       if txt_string.startswith('v=spf1'):
+         return txt_string.replace('v=spf1','')
+
+full_spf=getspf(domain)
+print 'Initial SPF string : ', full_spf
 
 while (full_spf.find('include:') > 0):
   for item in full_spf.split(' '):
     if item.startswith('include:'):
       sec_domain=item.replace('include:','')
-      answers = dns.resolver.query(sec_domain, 'TXT')
-      for rdata in answers:
-        for txt_string in rdata.strings:
-          if txt_string.startswith('v=spf'):
-            txt_string=txt_string.replace('v=spf1','')
-            print "SPF entry of %s : %s" % (sec_domain, txt_string)
-            full_spf=full_spf.replace(item,txt_string)
+      sec_spf=getspf(sec_domain)
+      sec_spf=re.sub(' .all','',sec_spf)
+      full_spf=full_spf.replace(item,sec_spf)
+      print "SPF entry of %s : %s" % (sec_domain, sec_spf)
 
 print 'Full SPF : ', full_spf
 
